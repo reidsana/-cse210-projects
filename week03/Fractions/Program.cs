c59ba9822262945d75739216c10bc0365c6034a2
reidsana <oksanareid48@gmail.com>
2025-11-14 18:03:02 -0500

    week3

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,60 @@
 using System;
 
-class Program
+namespace Fractions
 {
-    static void Main(string[] args)
+    public class Fraction
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        private int _top;
+        private int _bottom;
+
+       
+        public Fraction()
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+
+               public Fraction(int top)
+        {
+            _top = top;
+            _bottom = 1;
+        }
+
+                public Fraction(int top, int bottom)
+        {
+            _top = top;
+            _bottom = bottom;
+        }
+
+                public int GetTop()
+        {
+            return _top;
+        }
+
+        public void SetTop(int top)
+        {
+            _top = top;
+        }
+
+        public int GetBottom()
+        {
+            return _bottom;
+        }
+
+        public void SetBottom(int bottom)
+        {
+            _bottom = bottom;
+        }
+
+                public string GetFractionString()
+        {
+            return _top + "/" + _bottom;
+        }
+
+        
+        public double GetDecimalValue()
+        {
+            return (double)_top / _bottom;
+        }
     }
 }
