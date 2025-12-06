495e8ec0584791cae0ead2b7e201fb459d6dbb31
reidsana <oksanareid48@gmail.com>
2025-12-05 22:13:25 -0500

    week6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create individual shapes (optional testing)
+        Square s = new Square("Red", 4);
+        Rectangle r = new Rectangle("Blue", 5, 3);
+        Circle c = new Circle("Green", 2);
+
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine(s.GetArea());
+        Console.WriteLine();
+
+        Console.WriteLine(r.GetColor());
+        Console.WriteLine(r.GetArea());
+        Console.WriteLine();
+
+        Console.WriteLine(c.GetColor());
+        Console.WriteLine(c.GetArea());
+        Console.WriteLine();
+
+        // Build a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        Console.WriteLine("List of shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}  Area: {shape.GetArea()}");
+        }
     }
 }
