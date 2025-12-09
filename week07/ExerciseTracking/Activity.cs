fdd8abfec6dfce7b82f525342b6a354328257ee1
reidsana <oksanareid48@gmail.com>
2025-12-09 17:09:31 -0500

    week7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,36 @@
+using System;
+
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    // Polymorphic methods
+    public abstract double GetDistance(); // miles
+    public abstract double GetSpeed();    // mph
+    public abstract double GetPace();     // min per mile
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min) - " +
+               $"Distance {GetDistance():0.0} miles, " +
+               $"Speed {GetSpeed():0.0} mph, " +
+               $"Pace {GetPace():0.0} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running("03 Nov 2022", 30, 3.0);
+        Cycling cycle = new Cycling("04 Nov 2022", 45, 15.0);
+        Swimming swim = new Swimming("05 Nov 2022", 25, 20);
+
+        activities.Add(run);
+        activities.Add(cycle);
+        activities.Add(swim);
+
+        foreach (Activity a in activities)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
+
